4430f40f106b185c530e39a680fff7cf266ed277
Restu-Averian <restuaverianputra123@gmail.com>
2024-01-05 06:14:56 +0700

    temp: remove styledcomponentsregistry

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,11 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StyledComponentsRegistry>
-          <ConfigProvider>
-            <Layout>{children}</Layout>
-          </ConfigProvider>
-        </StyledComponentsRegistry>
+        {/* <StyledComponentsRegistry> */}
+        <ConfigProvider>
+          <Layout>{children}</Layout>
+        </ConfigProvider>
+        {/* </StyledComponentsRegistry> */}
       </body>
     </html>
   );
