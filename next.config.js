6c43b7c528182178961ce370eab80b5956329426
Restu-Averian <restuaverianputra123@gmail.com>
2024-01-10 22:39:07 +0700

    set redirect homepage to /ppcp

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/ppcp",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
