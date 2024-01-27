d37ab5512f8a88c24cc16e1665ef4fd58654972e
Restu-Averian <restuaverianputra123@gmail.com>
2024-01-27 15:02:36 +0700

    add suspense in content docs ppcp

--- src/app/ppcp/page.tsx
+++ src/app/ppcp/page.tsx
@@ -3,12 +3,13 @@
 import dynamic from "next/dynamic";
 import Loading from "../lib/Loading";
 import { tableDataProps } from "@/app/lib/ContentDocs";
+import { Suspense } from "react";
 
 const ContentDocs = dynamic(() => import("@/app/lib/ContentDocs"), {
-  // ssr: false,
+  ssr: false,
   loading: () => <Loading />,
 });
-const LofiAlbum = () => {
+const PPCP = () => {
   const queryData: tableDataProps[] = [
     {
       parameter: "num",
@@ -20,12 +21,14 @@ const LofiAlbum = () => {
   ];
 
   return (
-    <ContentDocs
-      baseUrl="https://ppcp-api-scrape.vercel.app/api?num=1"
-      description="Api untuk generate pp couple"
-      title="PP Couple"
-      tableData={queryData}
-    />
+    <Suspense>
+      <ContentDocs
+        baseUrl="https://ppcp-api-scrape.vercel.app/api?num=1"
+        description="Api untuk generate pp couple"
+        title="PP Couple"
+        tableData={queryData}
+      />
+    </Suspense>
   );
 };
-export default LofiAlbum;
+export default PPCP;
